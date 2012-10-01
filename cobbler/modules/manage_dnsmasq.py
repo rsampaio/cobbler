8b20773b25f8d3e24431c2e130008bafe80c4b32
diego souza <dsouza@c0d3.xxx>
2012-10-01 18:54:06 -0300

    bugfix: dhcp_tag only when netboot enabled

--- cobbler/modules/manage_dnsmasq.py
+++ cobbler/modules/manage_dnsmasq.py
@@ -137,9 +137,10 @@ class DnsmasqManager:
                     else:
                         systxt = ""
 
-                dhcp_tag = interface["dhcp_tag"]
-                if dhcp_tag == "":
-                   dhcp_tag = profile.dhcp_tag
+                if (system.netboot_enabled):
+                    dhcp_tag = interface["dhcp_tag"]
+                    if dhcp_tag == "":
+                       dhcp_tag = profile.dhcp_tag
                 if dhcp_tag == "":
                    dhcp_tag = "default"
 
